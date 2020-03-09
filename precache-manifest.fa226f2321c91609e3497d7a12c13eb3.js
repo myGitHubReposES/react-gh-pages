5f1dfd598dc84cb903f04027e952b33edd1534ab
felix.cordero <felix.cordero@vilt-group.com>
2020-03-09 12:41:48 +0100

    Updates

---
+++ precache-manifest.fa226f2321c91609e3497d7a12c13eb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f7ced76b076c87925f90d0026c0201",
+    "url": "/webinar-react/index.html"
+  },
+  {
+    "revision": "0dcb5bfe6ddfa62edf1c",
+    "url": "/webinar-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e931437c064da95aafa",
+    "url": "/webinar-react/static/js/2.c15c758e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/webinar-react/static/js/2.c15c758e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dcb5bfe6ddfa62edf1c",
+    "url": "/webinar-react/static/js/main.5200ab9b.chunk.js"
+  },
+  {
+    "revision": "3a991d9145dee6aaa464",
+    "url": "/webinar-react/static/js/runtime-main.e50b51a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webinar-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
